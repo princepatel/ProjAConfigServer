84e57e362568dcd9a36387cad91cbbe3b34c4f75
Prashant Patel <ppatel@gmail.com>
2020-08-17 12:13:42 +0530

    First commit

---
+++ src/test/java/com/projectA/configserver/ProjAConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectA.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjAConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
